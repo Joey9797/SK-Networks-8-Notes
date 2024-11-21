7520cb6d78c394729b76983e8ddab3dfa7ad4e33
GOEUN JU <133327235+Goeun-Ju@users.noreply.github.com>
2024-11-21 14:00:22 +0900

    feat: [주고은] Singleton 생성 방법 [SKAI8-x]

--- python/jge/second/app/main_1.py
+++ python/jge/second/app/main_1.py
@@ -1,7 +1,16 @@
 from dice.entity.dice import Dice
+from dice.repository.dice_repository_impl import DiceRepositoryImpl
 
 print("Our Second Class")
 
 dice = Dice()
 dice.rollDice()
 dice.printResult()
+
+diceRepository = DiceRepositoryImpl.getInstance()
+print(f"실행됨?: {diceRepository}")
+
+diceRepository = DiceRepositoryImpl.getInstance()
+print(f": DiceRepository2: {diceRepository}")
+diceRepository = DiceRepositoryImpl.getInstance()
+print(f": DiceRepository2: {diceRepository}")
