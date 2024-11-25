5ee46ba211ac7675776aa6260e79d8726d44ab02
Sanghoon Lee <gcccompil3r@gmail.com>
2024-11-25 11:34:51 +0900

    feat: [이상훈] dictionary key, value 뽑기 [SKAI8-x]

--- python/notes/fourth/game/repository/game_repository_impl.py
+++ python/notes/fourth/game/repository/game_repository_impl.py
@@ -5,6 +5,8 @@ from game.repository.game_repository import GameRepository
 class GameRepositoryImpl(GameRepository):
     __instance = None
 
+    __gameList = []
+
     def __new__(cls):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
@@ -20,3 +22,20 @@ class GameRepositoryImpl(GameRepository):
 
     def start(self, playerNameList, eachPlayerDiceList):
         game = Game(playerNameList, eachPlayerDiceList)
+        self.__gameList.append(game)
+
+    def checkWinner(self):
+        game = self.__gameList[0]
+        gameMapInfo = game.getGameMap()
+
+        # Dictionary의 key 값 다 뽑기
+        gameMapKeyList = gameMapInfo.keys()
+        # Dictionary의 value 값 다 뽑기
+        gameMapValueList = gameMapInfo.values()
+        # Dictionary의 key, value 값 다 뽑기
+        keyValueList = list(gameMapInfo.items())
+        print(f"gameMapKeyList: {gameMapKeyList}")
+        print(f"gameMapValueList: {gameMapValueList}")
+        print(f"keyValueList: {keyValueList}")
+
+
