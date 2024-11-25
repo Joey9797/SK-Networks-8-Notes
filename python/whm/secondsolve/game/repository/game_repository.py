d8c5ff1aca246427a5279e2a7fb59101499aee1b
왕환민 <mini_wang@naver.com>
2024-11-25 15:49:51 +0900

    feat: [왕환민] 주사위 굴리기 [SKAI-x]

--- python/whm/secondsolve/game/repository/game_repository.py
+++ python/whm/secondsolve/game/repository/game_repository.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 
 class GameRepository(ABC):
-    # abstractmethod는 ABC클래스 내에 인터페이스를 선언 할 경우 사용
+
     @abstractmethod
-    def Play(self):
-    # 아무것도 하지 않음(pass)
+    def start(self,playerNameList,eachPlayerDiceList):
+        pass
+
+    @abstractmethod
+    def checkWinner(self):
         pass
