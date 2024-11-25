dcbe1b496d086e569700ad843ae00c8a9dad6846
Joey9797 <pjhch1009@gmail.com>
2024-11-25 15:09:36 +0900

    feat: [박주희] fourth 파일 정리 [SKAI8-x]

--- python/pjh/fourth2/game/entity/game.py
+++
@@ -1,8 +0,0 @@
-class Game:
-    gameMap = {}
-
-    def __init__(self, playerList, eachPlayerDiceList):
-        for player, eachPlayerDice in zip(playerList, eachPlayerDiceList):
-            self.gameMap[player] = eachPlayerDice
-
-        print(f"self.gameMap: {self.gameMap}")
