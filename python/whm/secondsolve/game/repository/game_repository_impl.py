f891d5d6b97caf198ed3a8d232d245d79c32654c
왕환민 <mini_wang@naver.com>
2024-11-26 19:22:34 +0900

    feat:[왕환민] 주사위 게임 구현하기[SKAI8-x]

--- python/whm/secondsolve/game/repository/game_repository_impl.py
+++ python/whm/secondsolve/game/repository/game_repository_impl.py
@@ -1,10 +1,12 @@
+from dice.entity.dice_skill import DiceSkill
 from game.entity.game import Game
 from game.repository.game_repository import GameRepository
 
 
 class GameRepositoryImpl(GameRepository):
     __instance = None
-    __gameList=[]
+    __game= None
+
     def __new__(cls):
         #진짜 생성자 생성
         if cls.__instance is None:
@@ -20,43 +22,32 @@ class GameRepositoryImpl(GameRepository):
         return cls.__instance
     #싱글톤 생성
 
-    def start(self,playerNameList,eachPlayerDiceList):
-        game= Game(playerNameList,eachPlayerDiceList)
-        self.__gameList.append(game)
 
-    def checkWinner(self):
-        game=self.__gameList[0]
-        gameMapINfo=game.getGameMap()
+    def create(self):
+        while True:
+            try:
+                playerCount=int(input("몇명이십니까?"))
+                if playerCount<=1:
+                    print("2인 이상 게임입니다.")
+                    continue
+                game=Game(playerCount)
+                self.__game=game
+                break
 
-        for player,dicenumber in gameMapINfo.items():
-            print(player,"number is",dicenumber)
+            except ValueError:
+                print("숫자로 적으십시오!")
 
-        #Dictionary의 key값 다 뽑기
-        gameMapKeyList=gameMapINfo.keys()
-        # Dictionary의 value값 다 뽑기
-        gameMapValueList=gameMapINfo.values()
-        # Dictionary의 key,value값 다 뽑기
-        keyValueList=list(gameMapINfo.items())
+    def setplayerIndexDiceGameMap(self,playerIndexLsit,diceIdList):
+        self.__game.setplayerIndexDiceGameMap(playerIndexLsit,diceIdList)
 
+    def updatePlayerIndexListMap(self,skillAppliedPlayerIndexList,secondDiceIdList):
+        self.__game.updatePlayerIndexListMap(skillAppliedPlayerIndexList,secondDiceIdList)
 
-        #print(f"gameMapKeyList: {gameMapKeyList}")
-        #print(f"gameMapValueList: {gameMapValueList}")
-        #print(f"KeyValueList: {keyValueList}")
+    def deleteTargetPlayerId(self,targetPlayerId):
+        self.__game.deleteTargetPlayerId(targetPlayerId)
 
-        #람다 방식은 자체적으로 리스트나 어떤 반복적인 요소에서 개별적인 요소를 쪼개서 진행
-        #keys로 player객체를 선택하고 거기있는 Dice의 번호를 가져와서 비교시키는 코드
-        #Dictionary에서 value 가져오는 부분 ->gameMApINfo[player]
-        winner=max(gameMapINfo,key=lambda player:gameMapINfo[player].getDiceNumber())
-        #map에서 각각의 key,value쌍을 순회하면서 아래의 if조건에 만족하는 정보만 추려낸다
-        maxPlayerList=[player for player, dice in gameMapINfo.items()
-                       if dice.getDiceNumber()==gameMapINfo[winner].getDiceNumber()]
+    def getGamePlayerCount(self):
+        return self.__game.getPlayerCount()
 
-        maxPlayerCount = len(maxPlayerList)
-        if maxPlayerCount > 1:
-            print("무승부입니다")
-            return
-
-        print(f"winner: {winner}")
-
-
-
+    def getGame(self):
+        return self.__game
