c6f39ce2bd9df06fbe1412505f530e05eeb87ec3
ChoiByuengJun <gs3687@naver.com>
2024-11-27 12:30:32 +0900

    feat: [최병준] manmna1-1 [SKAI8-x]

---
+++ python/cbj/manmans/app/main.py
@@ -0,0 +1,5 @@
+from fruit.entity.fruit import fruitList
+from fruit.entity.fruit import fruitCountList
+
+fruitList = fruitList.getInstance()
+fruitCountList = fruitCountList()
---
+++ python/cbj/manmans/fruit/entity/fruit.py
@@ -0,0 +1,12 @@
+class Fruit:
+    def __init__(self, fruitList, fruitCountList):
+        self.fruitList = fruitList
+        self.fruitCountList = fruitCountList
+
+
+    fruitList = ["apple, orange, pear"]
+    fruitCountList = ["apple: 3, orange: 5, pear: 0"]
+
+    def f_print(self):
+        print("fruitList, fruitListCount")
+
