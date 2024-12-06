29956fe9f0c413677c619b189c344e044f6f7f16
Sanghoon Lee <gcccompil3r@gmail.com>
2024-12-06 10:55:06 +0900

    feat: [이상훈] 차량 등록 현황 출력 [SKAI8-x]

--- streamlit/notes/car_ui/cars/presentation/car_ui.py
+++ streamlit/notes/car_ui/cars/presentation/car_ui.py
@@ -6,7 +6,6 @@ from cars.domain.usecase import GetCarsUseCase
 from cars.infra.car_repository_impl import CarRepositoryImpl
 
 def showCarUi():
-    st.sidebar.title("Filter")
     car_repo = CarRepositoryImpl()
     usecase = GetCarsUseCase(car_repo)
 
@@ -15,17 +14,6 @@ def showCarUi():
 
         st.header("전체 데이터 예시")
         st.table(df)
-
-        select_multi = st.sidebar.multiselect("확인하고자 하는 차량 정보를 선택해주세요.", df.columns.tolist())
-        start_button = st.sidebar.button("차량 정보 조회")
-
-        if start_button:
-            if select_multi:
-                selected_df = df[select_multi]
-                st.header("조회된 차량 정보")
-                st.table(selected_df)
-            else:
-                st.warning("조회할 항목을 선택하세요.")
 
     except Exception as e:
         st.error(f"차량 데이터를 불러오지 못했습니다: {e}")
--- streamlit/notes/car_ui/charging/presentation/charging_ui.py
+++ streamlit/notes/car_ui/charging/presentation/charging_ui.py
@@ -11,16 +11,8 @@ def showChargingUi():
 
     try:
         df = usecase.execute()
-        select_multi = st.sidebar.multiselect("확인하고자 하는 요금 정보를 선택해주세요.", df.columns.tolist())
 
         st.table(df)
-        start_button = st.sidebar.button("요금 조회")
 
-        if start_button:
-            if select_multi:
-                selected_df = df[select_multi]
-                st.table(selected_df)
-            else:
-                st.warning("조회할 항목을 선택하세요.")
     except Exception as e:
         st.error(f"충전 요금 데이터를 불러오지 못했습니다: {e}")
