20ca1c65994ff29153d9e36f547d4a23a8d75f60
ChoiByuengJun <gs3687@naver.com>
2024-12-10 17:40:45 +0900

    feat: [최병준] pandas 데이터 통계 요약 [SKAI8-x]

--- django/cbj/db_automation/dice/urls.py
+++ django/cbj/db_automation/dice/urls.py
@@ -9,15 +9,15 @@ router.register(r"dice", DiceController, basename='dice')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('request-roll-dice',
-         # 웹 브라우저 상에서 /request-roll-dice 요청이 오면 'requestRollDice()구동
+    path('request-roll-dice', 
+         # 웹 브라우저 상에서 /request-roll-dice 요청이 오면 'requestRollDice()' 구동
          # 결론적으로 /dice/request-roll-dice 로 시작
          DiceController.as_view({ 'get': 'requestRollDice' }),
          name='주사위 굴리기'),
     path('request-find-dice',
-         DiceController.as_view({'get': 'requestFindDice'}),
+         DiceController.as_view({ 'get': 'requestFindDice' }),
          name='주사위 id로 찾기'),
     path('request-every-dice',
-         DiceController.as_view({ 'get': 'requestEverDice'}),
+         DiceController.as_view({ 'get': 'requestEveryDice' }),
          name='주사위 전체 정보 획득'),
 ]
