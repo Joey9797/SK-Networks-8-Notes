b95af138baf965aeef698c4ddc3d7fb1b2ec6bcd
ChoiByuengJun <gs3687@naver.com>
2024-12-12 17:12:53 +0900

    feat: [최병준] pandas basic 정보 획득[SKAI8-x]

--- nuxt/cbj/ui/pandas_basic/pages/PandasBasicInfo.vue
+++ nuxt/cbj/ui/pandas_basic/pages/PandasBasicInfo.vue
@@ -1,6 +1,17 @@
 <template>
     <v-container>
-        <p>일단 잘 나오는지 확인해보자</p>
+        <h2>Pandas Basic Info</h2>
+        <v-list>
+            <v-list-item
+                v-for="pandasInfo in pandasInfoList"
+                :key="pandasInfo.id"
+            >
+                <v-list-item-content>
+                    <v-list-item-title>{{ pandasInfo.name }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ pandasInfo.age }}</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
     </v-container>
 </template>
 
@@ -8,8 +19,37 @@
 import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
-    setup() {
-        
+    setup () {
+        const pandasInfoList = ref([])
+        const loading = ref(false)
+        const error = ref(null)
+
+        const fetchPandasInfo = async () => {
+            loading.value = true
+            try {
+                const response = await fetch(
+                    'http://localhost:8000/pandas-basic/request-pandas-info')
+
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`)
+                }
+
+                const data = await response.json()
+                pandasInfoList.value = data.serializedPandasInfoList
+            } catch (err) {
+                error.value = err.message
+            } finally {
+                loading.value = false
+            }
+        }
+
+        onMounted(fetchPandasInfo)
+
+        return {
+            pandasInfoList,
+            loading,
+            error,
+        }
     },
 })
 </script>
