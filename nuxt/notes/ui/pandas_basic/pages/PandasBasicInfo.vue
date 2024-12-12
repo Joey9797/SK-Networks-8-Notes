67d1a0cc821ba266328d751322c8b023612e9d1a
Sanghoon Lee <gcccompil3r@gmail.com>
2024-12-12 14:27:09 +0900

    init: [이상훈] Nuxt 페이지 초기화 [SKAI8-x]

---
+++ nuxt/notes/ui/pandas_basic/pages/PandasBasicInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <h2>Pandas Basic Info</h2>
+        <v-list>
+            <v-list-item
+                v-for="pandasInfo in pandasInfoList"
+                :key="pandasInfo.id"
+            >
+                <v-list-item-content>
+                    
+                </v-list-item-content>
+            </v-list-item>
+    </v-container>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
