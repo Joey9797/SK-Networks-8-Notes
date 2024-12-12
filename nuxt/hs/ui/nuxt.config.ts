1dbcf246cd1ef6364ae97f3724727891c365b2c8
Hyeonseo2083 <122842851+jungs0914@users.noreply.github.com>
2024-12-12 17:11:46 +0900

    feat: [정현서] pandas basic 정보 획득[SKAI8-x]

--- nuxt/hs/ui/nuxt.config.ts
+++ nuxt/hs/ui/nuxt.config.ts
@@ -1,6 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config"
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module']
+  extends: ['./pandas_basic/nuxt.config.ts'],
+
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
+
+  build: {
+    transpile: ['vuetify']
+  },
+
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+  },
+
+  modules: [
+    'vuetify-nuxt-module',
+    '@pinia/nuxt',
+    '~/pandas_basic/index.ts'
+  ],
+
+  imports: {
+    dirs: ['./stores']
+  }
 })
