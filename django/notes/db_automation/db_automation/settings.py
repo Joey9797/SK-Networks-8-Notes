906b87b5875f8bd98f0fb4883e576b5511f8e3e2
Sanghoon Lee <gcccompil3r@gmail.com>
2024-12-13 12:08:30 +0900

    feat: [이상훈] 카카오 로그인 요청 [SKAI8-x]

--- django/notes/db_automation/db_automation/settings.py
+++ django/notes/db_automation/db_automation/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'car_business_pricing',
     'pandas_basic',
     'excel_basic',
+    'kakao_authentication',
 ]
 
 MIDDLEWARE = [
@@ -91,6 +92,14 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
     'x-request-with',
 ]
+
+KAKAO = {
+    'LOGIN_URL': os.getenv('KAKAO_LOGIN_URL'),
+    'CLIENT_ID': os.getenv('KAKAO_CLIENT_ID'),
+    'REDIRECT_URI': os.getenv('KAKAO_REDIRECT_URI'),
+    'TOKEN_REQUEST_URI': os.getenv('KAKAO_TOKEN_REQUEST_URI'),
+    'USER_INFO_REQUEST_URI': os.getenv('KAKAO_USER_INFO_REQUEST_URI'),
+}
 
 ROOT_URLCONF = "db_automation.urls"
 
