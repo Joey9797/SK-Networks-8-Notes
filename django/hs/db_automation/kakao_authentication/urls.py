3811e01659c3783f4f647b6f754471939eea4430
Hyeonseo2083 <122842851+jungs0914@users.noreply.github.com>
2024-12-17 17:53:48 +0900

    feat:[정현서] 사이드바 생성 [SKAI8-x]

--- django/hs/db_automation/kakao_authentication/urls.py
+++ django/hs/db_automation/kakao_authentication/urls.py
@@ -8,7 +8,10 @@ router.register(r"kakao-oauth", KakaoOauthController, basename='kakao-oauth')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('kakao',
+    path('request-login-url',
          KakaoOauthController.as_view({ 'get': 'requestKakaoOauthLink' }),
          name='Kakao Oauth 링크 요청'),
+    path('redirect-access-token',
+         KakaoOauthController.as_view({ 'post': 'requestAccessToken' }),
+         name='Kakao Access Token 요청'),
 ]
