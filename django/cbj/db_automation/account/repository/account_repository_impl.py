bd59e03713fd69cc3a0e411e7a2e6654a6a883ab
ChoiByuengJun <gs3687@naver.com>
2024-12-17 09:04:04 +0900

    feat:[최병준] token access 구현[SKAI8-x]

---
+++ django/cbj/db_automation/account/repository/account_repository_impl.py
@@ -0,0 +1,23 @@
+from account.entity.account import Account
+from account.repository.account_repository import AccountRepository
+
+
+class AccountRepositoryImpl(AccountRepository):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def save(self, email):
+        account = Account(email=email)
+        return account.save()
