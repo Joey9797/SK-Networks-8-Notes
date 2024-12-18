195a12d5a80f36a3f8e43c153cc31f77c1068d3b
제나 <denalog@naver.com>
2024-12-18 14:04:52 +0900

    feat: [유제나] 게임 소프트웨어 리스트 요청 [SKAI8-x]

--- django/rjn/db_automation/game_software/serializer/game_software_list_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from game_software.entity.game_software import GameSoftware
-
-
-class GameSoftwareListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GameSoftware
-        fields = ['id', 'title', 'price', 'image']
